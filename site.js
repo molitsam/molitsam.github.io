1dc72bc0f379fc36389308e85c65fc825f8e931e
Sade Molitor <500169348@fvtc.edu>
2024-02-11 22:29:04 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,2 +1,22 @@
 //this makes an annoying alert pop-up on the site
-alert('Hello')
+//alert('Hello')
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+if (isMorning){ //alert('good morning')
+const p = welcome.querySelector('p')
+p.textContent = "Good Morning!"
+}
+
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+if (isAfternoon){ //alert('good morning')
+    const p = welcome.querySelector('p')
+    p.textContent = "Good Afternoon!"
+    }
+
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+if (isEvening){ //alert('good morning')
+    const p = welcome.querySelector('p')
+    p.textContent = "Good Evening!"
+    }
